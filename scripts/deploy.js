13907f78f05569a00a3b2b2c3716b46815bbb485
Justin <justin7359900@gmail.com>
2023-03-13 14:08:54 -0400

    Adds starter code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,76 +5,7 @@ const tokens = (n) => {
 }
 
 async function main() {
-  // Setup accounts & variables
-  const [deployer] = await ethers.getSigners()
-  const NAME = "TokenMaster"
-  const SYMBOL = "TM"
 
-  // Deploy contract
-  const TokenMaster = await ethers.getContractFactory("TokenMaster")
-  const tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
-  await tokenMaster.deployed()
-
-  console.log(`Deployed TokenMaster Contract at: ${tokenMaster.address}\n`)
-
-  // List 6 events
-  const occasions = [
-    {
-      name: "UFC Miami",
-      cost: tokens(3),
-      tickets: 0,
-      date: "Apr 8",
-      time: "6:00PM EST",
-      location: "Miami-Dade Arena - Miami, FL"
-    },
-    {
-      name: "ETH Tokyo",
-      cost: tokens(1),
-      tickets: 125,
-      date: "Apr 14",
-      time: "1:00PM JST",
-      location: "Tokyo, Japan"
-    },
-    {
-      name: "ETH Privacy Hackathon",
-      cost: tokens(0.25),
-      tickets: 200,
-      date: "Apr 27",
-      time: "10:00AM TRT",
-      location: "Turkey, Istanbul"
-    },
-    {
-      name: "Dallas Mavericks vs. San Antonio Spurs",
-      cost: tokens(5),
-      tickets: 0,
-      date: "May 2",
-      time: "2:30PM CST",
-      location: "American Airlines Center - Dallas, TX"
-    },
-    {
-      name: "ETH Global Toronto",
-      cost: tokens(1.5),
-      tickets: 125,
-      date: "Jun 23",
-      time: "11:00AM EST",
-      location: "Toronto, Canada"
-    }
-  ]
-
-  for (var i = 0; i < 5; i++) {
-    const transaction = await tokenMaster.connect(deployer).list(
-      occasions[i].name,
-      occasions[i].cost,
-      occasions[i].tickets,
-      occasions[i].date,
-      occasions[i].time,
-      occasions[i].location,
-    )
-
-    await transaction.wait()
-
-    console.log(`Listed Event ${i + 1}: ${occasions[i].name}`)
-  }
 }
 
 main().catch((error) => {
--- src/App.js
+++ src/App.js
@@ -14,78 +14,14 @@ import TokenMaster from './abis/TokenMaster.json'
 import config from './config.json'
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
-
-  const [tokenMaster, setTokenMaster] = useState(null)
-  const [occasions, setOccasions] = useState([])
-
-  const [occasion, setOccasion] = useState({})
-  const [toggle, setToggle] = useState(false)
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-
-    const network = await provider.getNetwork()
-    const tokenMaster = new ethers.Contract(config[network.chainId].TokenMaster.address, TokenMaster, provider)
-    setTokenMaster(tokenMaster)
-
-    const totalOccasions = await tokenMaster.totalOccasions()
-    const occasions = []
-
-    for (var i = 1; i <= totalOccasions; i++) {
-      const occasion = await tokenMaster.getOccasion(i)
-      occasions.push(occasion)
-    }
-
-    setOccasions(occasions)
-
-    window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const account = ethers.utils.getAddress(accounts[0])
-      setAccount(account)
-    })
-  }
-
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
 
   return (
     <div>
       <header>
-        <Navigation account={account} setAccount={setAccount} />
 
-        <h2 className="header__title"><strong>Event</strong> Tickets</h2>
+        <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
       </header>
 
-      <Sort />
-
-      <div className='cards'>
-        {occasions.map((occasion, index) => (
-          <Card
-            occasion={occasion}
-            id={index + 1}
-            tokenMaster={tokenMaster}
-            provider={provider}
-            account={account}
-            toggle={toggle}
-            setToggle={setToggle}
-            setOccasion={setOccasion}
-            key={index}
-          />
-        ))}
-      </div>
-
-      {toggle && (
-        <SeatChart
-          occasion={occasion}
-          tokenMaster={tokenMaster}
-          provider={provider}
-          setToggle={setToggle}
-        />
-      )}
     </div>
   );
 }
