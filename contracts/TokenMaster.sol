8a8581a1dbd15f34b3f9f4c9bb17ff7d2c971c23
JackWang-60971031 <60971031H@gapps.ntnu.edu.tw>
2023-07-28 15:06:41 +0800

    更新完成

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TokenMaster is ERC721 { // is關鍵字用來只指定TokenMaster合約繼承ERC721合約
+contract TokenMaster is ERC721 { // is關鍵字用來只指定TokenMaster合約繼承ERC721合約的函式等
     
+    constructor(
+        string memory _name, 
+        string memory _symbol
+    ) ERC721(_name, _symbol){ // TokenMaster合約的建構函式的兩個參數會分別傳遞至ERC721合約的建構函式中(ERC721(_name, _symbol))
+
+    }
     
 }
